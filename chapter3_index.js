b906bce09add065acabc9db790828a906013b0fa
Agus RezaAristiadi Nurwa <agus.reza@gmail.com>
2018-08-18 13:13:55 +0700

    sebelum pindah ke section mongoDB

--- chapter3_index.js
+++ chapter3_index.js
@@ -7,22 +7,12 @@ const app = express();
 
 console.log('before passport.use');
 
-// const log4js = require('log4js');
-// log4js.configure({
-// 	appenders: { cheese: { type: 'file', filename: 'cheese.log' } },
-// 	categories: { default: { appenders: ['cheese'], level: 'debug' } }
-// });
-//
-// const logger = log4js.getLogger('cheese');
-// logger.debug('tessss');
-
 passport.use(
 	new GoogleStrategy(
 		{
 			clientID: keys.googleClientID,
 			clientSecret: keys.googleClientSecret,
 			callbackURL: '/auth/google/callback'
-			// callbackURL: 'http://www.lalareza.com'
 		},
 		(accessToken, refreshToken, profile, done) => {
 			console.log('test testttt');
@@ -39,8 +29,6 @@ app.get(
 		scope: ['profile', 'email']
 	})
 );
-
-app.get('/auth/google/callback', passport.authenticate('google'));
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
