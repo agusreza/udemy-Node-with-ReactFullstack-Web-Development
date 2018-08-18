1b1dff2b20468fb6f66a963f8810e6d3b5377582
Agus RezaAristiadi Nurwa <agus.reza@gmail.com>
2018-08-18 16:43:16 +0700

    refactor fungsi ke masing2 file

--- index.js
+++ index.js
@@ -1,46 +1,10 @@
 const express = require('express');
-const passport = require('passport');
-const GoogleStrategy = require('passport-google-oauth20').Strategy;
-const keys = require('./config/keys'); // ga usah pake .js extension
+require('./services/passport');
 
 const app = express();
 
-console.log('before passport.use');
-
-// const log4js = require('log4js');
-// log4js.configure({
-// 	appenders: { cheese: { type: 'file', filename: 'cheese.log' } },
-// 	categories: { default: { appenders: ['cheese'], level: 'debug' } }
-// });
-//
-// const logger = log4js.getLogger('cheese');
-// logger.debug('tessss');
-
-passport.use(
-	new GoogleStrategy(
-		{
-			clientID: keys.googleClientID,
-			clientSecret: keys.googleClientSecret,
-			callbackURL: '/auth/google/callback'
-			// callbackURL: 'http://www.lalareza.com'
-		},
-		(accessToken, refreshToken, profile, done) => {
-			console.log('test testttt');
-			console.log('access token', accessToken);
-			console.log('refresh token', refreshToken);
-			console.log('profile', profile);
-		}
-	)
-);
-
-app.get(
-	'/auth/google',
-	passport.authenticate('google', {
-		scope: ['profile', 'email']
-	})
-);
-
-app.get('/auth/google/callback', passport.authenticate('google'));
+require('./routes/authRoutes')(app);
+//console.log('before passport.use');
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
