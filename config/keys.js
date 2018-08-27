77da6a95fdc675459cf38f4df8a92efee412eab9
Agus RezaAristiadi Nurwa <agus.reza@gmail.com>
2018-08-27 17:06:05 +0700

    finished oauth flow

---
+++ config/keys.js
@@ -0,0 +1,8 @@
+// keys.js
+if (process.env.NODE_ENV === 'production') {
+	// berarti ini di production
+	module.exports = require('./prod');
+} else {
+	// berarti ini di development environtment
+	module.exports = require('./dev');
+}
